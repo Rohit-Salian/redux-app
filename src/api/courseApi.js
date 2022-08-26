adb2e2dfd792da710db8a1cb421f66db3eba723f
Rohit Salian <salian.rohit1997@gmail.com>
2022-08-26 15:28:40 +0530

    header edit

--- src/api/courseApi.js
+++ src/api/courseApi.js
@@ -1,17 +1,26 @@
-import { handleResponse, handleError } from "./apiUtils";
+import { handleError, handleResponse } from "./apiUtils";
 const baseUrl = process.env.API_URL + "/courses/";
 
 export function getCourses() {
-  return fetch(baseUrl)
-    .then(handleResponse)
-    .catch(handleError);
+  return (
+    fetch(baseUrl).then(handleResponse).catch(handleError),
+    {
+      headers: {
+        "content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  );
 }
 
 export function saveCourse(course) {
   return fetch(baseUrl + (course.id || ""), {
-    method: course.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify(course)
+    method: course.id ? "PUT" : "POST",
+    headers: {
+      "content-type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(course),
   })
     .then(handleResponse)
     .catch(handleError);
